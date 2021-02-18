4c8bfc7206f670d32dabf18994a82a07dd57bcdc
vivekbhadula <vivekbhadula201420@gmail.com>
2021-02-18 21:49:50 +0530

    Added a countdown timer and printed the value of the key pressed.

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,9 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    let eggTimes = ["Soft":300, "Medium":420, "Hard":720]
+    
     @IBAction func hardnessSelected(_ sender: UIButton) {
-        print(sender.currentTitle!)
+        let hardness = sender.currentTitle!
+        let result = eggTimes[hardness]!
+        print(result)
+        
+        Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+
+        
     }
     
+    var counter = 60
 
+    @objc func updateCounter() {
+        //example functionality
+        if counter > 0 {
+            print("\(counter) seconds")
+            counter -= 1
+        }
+    }
+    
 }
+
+
