242c279454193f67d138cc747840896183d4e5a8
vivekbhadula <vivekbhadula201420@gmail.com>
2021-02-19 13:19:34 +0530

    Used timer.invalidate() and printed DONE when the timer is up.

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,22 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let eggTimes = ["Soft":300, "Medium":420, "Hard":720]
+    let eggTimes = ["Soft":3, "Medium":4, "Hard":7]
     var counter = 0
+    var timer = Timer()
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
+//        titleLabel.text = "How do you like your eggs?"
+        timer.invalidate()
         let hardness = sender.currentTitle!
         counter = eggTimes[hardness]!
         
-        Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
     }
     
-
+    @IBOutlet weak var titleLabel: UILabel!
+    
     @objc func updateCounter() {
         //example functionality
         if counter > 0 {
             print("\(counter) seconds")
             counter -= 1
+        }
+        else {
+            timer.invalidate()
+            titleLabel.text = "DONE"
         }
     }
     
